b3989435b02e0dc26969cab418af067eeacfff59
Chrislearn Young <chris@acroidea.com>
2025-03-20 20:53:39 +0800

    wip

--- http3/src/datagram/mod.rs
+++
@@ -1,9 +0,0 @@
-//! H3 Datagram
-
-mod datagram;
-pub use datagram::Datagram;
-mod datagram_traits;
-pub use datagram_traits::HandleDatagramsExt;
-mod quic_traits;
-pub use quic_traits::{RecvDatagramExt, SendDatagramExt};
-pub mod server;
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub use h3::*;
+
+pub mod webtransport;
+
+
+#[cfg(feature = "quinn")]
+pub use h3_quinn as quinn;
